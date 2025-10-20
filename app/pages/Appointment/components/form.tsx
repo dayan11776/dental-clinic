25ab15ae4ec46b04b445a437f7e7995f5148f93c
Bryan <bryantapel619@gmail.com>
2025-10-20 23:00:08 +0800

    Update

--- app/pages/Appointment/components/form.tsx
+++ app/pages/Appointment/components/form.tsx
@@ -39,16 +39,16 @@ export const Form = () => {
         <div className="grid gap-6 mb-6 md:grid-cols-2">
             <div>
                 <label htmlFor="first_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">First name</label>
-                <input type="text" name='firstname' id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
+                <input type="text" name='firstname' id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First name" required/>
             </div>
             <div>
                 <label htmlFor="last_name" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Last name</label>
-                <input type="text" name='lastname' id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+                <input type="text" name='lastname' id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last name" required />
             </div>
         </div>
         <div className="mb-6">
             <label htmlFor="email" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Email address<span className='text-gray-500 ml-2'>(required)</span></label>
-            <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com"  required/>
+            <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example123@company.com"  required/>
         </div> 
         <div className="mb-6">
             <label htmlFor="phone" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Phone number<span className='text-gray-500 ml-2'>(required)</span></label>
@@ -70,8 +70,14 @@ export const Form = () => {
             <label htmlFor="message" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">How can we help you?<span className='text-gray-500 ml-2'>(required)</span></label>
             <textarea id="message" name='message' rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your request here..."></textarea>
         </div>
-
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button 
+          type="submit"
+          className="bg-green-700 text-gray-100 
+                    hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 
+                    font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center 
+                    dark:bg-gray-200 dark:text-green-600 dark:hover:bg-gray-300 dark:focus:ring-gray-400">
+          Submit
+        </button>
     </form>
   )
 }
